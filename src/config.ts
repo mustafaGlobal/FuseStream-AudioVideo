f73e13d7ede7c288cc18ff5e8998faa0d4abaca7
Mustafa Spahic <mustafa.spahic.1t2@gmail.com>
2023-08-20 18:59:43 +0200

    Make it so it's video only service

--- src/config.ts
+++ src/config.ts
@@ -22,12 +22,6 @@ export const config = {
     router: {
       mediaCodecs: [
         {
-          kind: 'audio',
-          mimeType: 'audio/opus',
-          clockRate: 48000,
-          channels: 2,
-        },
-        {
           kind: 'video',
           mimeType: 'video/VP8',
           clockRate: 90000,
